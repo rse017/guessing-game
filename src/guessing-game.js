b5e41a29be8d5ed58c87ce11709bb816153b513c
Sergei R <rse017@gmail.com>
2023-02-15 22:59:25 +0300

    feat: test solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.res = Math.ceil((this.max + this.min) / 2);
+        return this.res;
     }
 
     lower() {
-
+       this.max = this.res ;
     }
 
     greater() {
-
+        this.min = this.res ;
     }
 }
-
 module.exports = GuessingGame;
